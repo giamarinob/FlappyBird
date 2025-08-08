2498c776990ec3ba7bf29797c5eb18861913f3ab
Benjamin Giamarino <giamarinob@gmail.com>
2025-08-08 13:35:48 -0500

    Refactor run environment to train indefinitely

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ import gymnasium
 import torch
 from dqn import DQN
 from experience_replay import ReplayMemory
+import itertools
 
 device = torch.device("mps" if torch.backends.mps.is_available() else (
     "cuda" if torch.cuda.is_available() else "cpu"))
@@ -10,27 +11,28 @@ device = torch.device("mps" if torch.backends.mps.is_available() else (
 class Agent:
     def run(self, is_training=True, render=False):
         env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None, use_lidar=False)
-
         num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
-
+        rewards_per_episode = []
         policy_dqn = DQN(env.observation_space.shape[0], num_actions).to_device(device)
 
         if is_training:
             memory = ReplayMemory(10000)
 
-        state, _ = env.reset()
-        while True:
-            # Next action:
-            # (feed the observation to your agent here)
-            action = env.action_space.sample()
+        for epsiode in itertools.count():
+            state, _ = env.reset()
+            terminated = False
+            episode_reward = 0.0
 
-            # Processing:
-            new_state, reward, terminated, _, info = env.step(action)
+            while not terminated:
+                # Next action:
+                # (feed the observation to your agent here)
+                action = env.action_space.sample()
 
-            state = new_state
-            # Checking if the player is still alive
-            if terminated:
-                break
+                # Processing:
+                new_state, reward, terminated, _, info = env.step(action)
+                episode_reward += reward
 
-        env.close()
+                state = new_state
+
+            rewards_per_episode.append(episode_reward)

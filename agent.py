f0125dc5cbf6263884744bfa3cbc83c3146e6984
Benjamin Giamarino <giamarinob@gmail.com>
2025-08-08 13:17:54 -0500

    Add basic dqn network

--- agent.py
+++ agent.py
@@ -1,19 +1,31 @@
 import flappy_bird_gymnasium
 import gymnasium
+import torch
+from dqn import DQN
 
-env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+device = torch.device("mps" if torch.backends.mps.is_available() else (
+    "cuda" if torch.cuda.is_available() else "cpu"))
 
-obs, _ = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space.sample()
+class Agent:
+    def run(self, is_training=True, render=False):
+        env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None, use_lidar=False)
 
-    # Processing:
-    obs, reward, terminated, _, info = env.step(action)
+        num_states = env.observation_space.shape[0]
+        num_actions = env.action_space.n
 
-    # Checking if the player is still alive
-    if terminated:
-        break
+        policy_dqn = DQN(env.observation_space.shape[0], env.action_space.n).to_device(device)
 
-env.close()
+        obs, _ = env.reset()
+        while True:
+            # Next action:
+            # (feed the observation to your agent here)
+            action = env.action_space.sample()
+
+            # Processing:
+            obs, reward, terminated, _, info = env.step(action)
+
+            # Checking if the player is still alive
+            if terminated:
+                break
+
+        env.close()

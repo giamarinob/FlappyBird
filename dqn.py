f894a7e30806142bac016880754726bc9650b53f
Benjamin Giamarino <giamarinob@gmail.com>
2025-08-19 17:51:24 -0500

    Implement Dueling DQN action and value layers

--- dqn.py
+++ dqn.py
@@ -10,12 +10,38 @@ class DQN(nn.Module):
 
         self.fc1 = nn.Linear(state_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, action_dim)
+
+        # DQN
+        # self.fc3 = nn.Linear(hidden_dim, action_dim)
+
+        # Dueling DQN
+        # Value stream
+        self.fc_value = nn.Linear(hidden_dim, hidden_dim)
+        self.value = nn.Linear(hidden_dim, 1)
+
+        # Advantages stream
+        self.fc_advantage = nn.Linear(hidden_dim, hidden_dim)
+        self.advantage = nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        return self.fc3(x)
+
+        # DQN
+        # return self.fc3(x)
+
+        # Dueling DQN
+        # Value Calc
+        fc_v = F.relu(self.fc_value(x))
+        value = self.value(fc_v)
+
+        # Advantage Calc
+        fc_a = F.relu(self.fc_advantage(x))
+        advantage = self.advantage(fc_a)
+
+        q = value + advantage - torch.mean(advantage, dim=1, keepdim=True)
+
+        return q
 
 
 if __name__ == "__main__":
